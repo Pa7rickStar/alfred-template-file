f93c76b4a5717bc24b83b8f52227936ca64c5906
Patrick Saile <contact@PatrickSaile.de>
2025-03-13 18:26:00 +0100

    added functionality for dotfiles and basefiles

--- new_file.sh
+++ new_file.sh
@@ -8,20 +8,57 @@
 if [[ -z "${out_dir}" ]]; then
   out_dir=${HOME}"/Desktop/"
 fi
-
-extension="${filename##*.}"
 out_path="${out_dir}${filename}"
+
+extension=""
+if [[ "$filename" == *.* && "$filename" != .* ]]; then
+  extension="${filename##*.}"
+fi
+
+if [[ -z "${template}" ]]; then
+  if [[ "$filename" == .* ]]; then
+    template=$(find "${src_dir}" -type f -name "${filename}" -print)
+  else
+    template=$(find "${src_dir}" -type f -name "*.${extension}" -print)
+  fi
+fi
 
 if [[ -f "${out_path}" ]]; then
   echo "${filename} already exists"
 else
-  template="${src_dir}/default_templates/${extension}.${extension}" 
-  if [[ -f "${template}" ]]; then
-    cp  "${template}" "${out_path}"
-    echo "${filename} created in ${out_dir}"
+  if [[ -n "$extension" || "$filename" == .* ]]; then
+    if [[ -z "$template" ]]; then
+      echo "Failed! Unsupported file type, please add template file first."
+    elif [[ $(echo "$template" | wc -l) -eq 1 ]]; then
+      cp "$template" "$out_path"
+      echo "${filename} created in ${out_dir}"
+      open "${out_dir}"
+    else
+      echo "{\"items\": ["
+      items=()
+      while IFS= read -r template_item; do
+          relative_path="${template_item#${src_dir}/}"
+          template_uid=$(basename "$template_item")
+          items+=( "{\"uid\": \"${template_uid}\", \"type\": \"file\", \"title\": \"${filename}\", \"subtitle\": \"../${relative_path}\", \"arg\": \"${template_item}\"}" )
+      done <<< "$template"
+      for (( i=0; i<${#items[@]}; i++ )); do
+        if [[ $i -lt $((${#items[@]} - 1)) ]]; then
+          printf "%s,\n" "${items[i]}"
+        else
+          printf "%s\n" "${items[i]}"
+        fi
+      done
+      echo "]}"
+    fi
   else
-    echo 'Failed! Unsupported file type, please add template file first.'
+    if [[ -d "${out_path}" ]]; then
+      echo "The file ${filename} can't' be created without a file extension because a folder called ${filename} already exists in ${out_dir}. "
+    else
+      touch "${out_path}"
+      echo "${filename} created in ${out_dir}"
+    fi
+    open "${out_dir}"
   fi
 fi
 
-open "${out_dir}"
+
